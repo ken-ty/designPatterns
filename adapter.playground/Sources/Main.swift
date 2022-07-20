e10d3ab3fffe1a2ce43e1da23abe57d90ab8f4ab
todoggler <kentokuraCS@gmail.com>
2022-07-20 22:00:28 +0900

    clean: 不要コード削除

--- adapter.playground/Sources/Main.swift
+++ adapter.playground/Sources/Main.swift
@@ -3,26 +3,5 @@ import Foundation
 /// 動作テスト
 public class Main {
     public init() {
-        // Aggregateを初期化
-        let samples = SampleAggregate()
-        // 集合にサンプル要素を追加していく
-        samples.append(Sample("吾輩は猫である"))
-        samples.append(Sample("舞姫"))
-        samples.append(Sample("羅生門"))
-        samples.deleteLast() // 羅生門を削除
-        samples.append(Sample("注文の多いレストラン"))
-
-        // イテレータ取得
-        let samplesIterator = samples.iterator()
-        
-        // イテレータ動作確認
-        // 上記で追加したサンプルを1つずつスキャンして名前を取得する
-        while samplesIterator.hasNext() {
-            guard let sample = samplesIterator.next() else {
-                fatalError("要素が取得できませんでした")
-            }
-            print(sample.getName())
-            sleep(1)
-        }
     }
 }
